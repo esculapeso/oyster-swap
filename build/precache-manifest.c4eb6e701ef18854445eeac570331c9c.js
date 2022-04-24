a6e98214f43ae2b98acfa65752b75cd27679f9cb
Esculap ESA <esculapesa@gmail.com>
2022-04-25 00:48:01 +0200

    added build files

---
+++ build/precache-manifest.c4eb6e701ef18854445eeac570331c9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a5487325e6a8ee08f89bde088ee45ad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "42f42fba890b7e035736",
+    "url": "./static/css/2.4b60f7c3.chunk.css"
+  },
+  {
+    "revision": "387a98dc8bff5a2f9778",
+    "url": "./static/css/main.02c749a2.chunk.css"
+  },
+  {
+    "revision": "42f42fba890b7e035736",
+    "url": "./static/js/2.0d6b7648.chunk.js"
+  },
+  {
+    "revision": "1a29f8fb7f6a758d9dce8a0780f3b302",
+    "url": "./static/js/2.0d6b7648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "387a98dc8bff5a2f9778",
+    "url": "./static/js/main.0d07b779.chunk.js"
+  },
+  {
+    "revision": "d852897cea0a694b3726",
+    "url": "./static/js/runtime-main.c06bb91b.js"
+  }
+]);
